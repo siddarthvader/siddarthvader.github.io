5abe6a8b29fcbd486b23a606ec4c1190dc6687ae
siddharth <siddharthjn93@gmail.com>
2016-12-27 21:48:21 +0530

    goals page updated

--- app/typescript/script.ts
+++ app/typescript/script.ts
@@ -1,6 +1,28 @@
-function greeter(str){
-    return "first one here"+str;
+class student{
+    fullName:string;
+    constructor(public firstName, public lastName){
+        this.fullName=firstName+lastName;
+    }
 }
 
-var user="name is bond";
+interface Person{
+    firstName:string,
+    lastName:string
+}
+
+function greeter(person : Person){
+    let color:string='blue';
+    let count:number=10;
+    let a:string=`Hello ${person.firstName+person.lastName}
+            and i'm going to be ${count+1} years old tomorrow `;
+    let list:number[]=[3,3,3];
+    let list1:Array<number>=[2,2,2];
+    let list2:[number,string]=[1,'new';
+    console.log(list2[0]);
+    console.log(a);
+    return a;
+}
+
+var user=new student('mary','jane');
+
 document.body.innerHTML=greeter(user);
