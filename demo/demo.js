1dd43aaf9595e86fc15115cc85653897a5406046
siddharth <siddharthjn93@gmail.com>
2017-01-24 00:49:26 +0530

    back cam support

--- demo/demo.js
+++ demo/demo.js
@@ -1,23 +1,125 @@
-// Grab elements, create settings, etc.
-var video = document.getElementById('video');
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-// Get access to the camera!
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-        video.src = window.URL.createObjectURL(stream);
-        video.play()
-        // context.drawImage(video, 0, 0, 640, 480);
-        console.info(video,"video--->");        
-        
-    });
+/*
+*  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+*
+*  Use of this source code is governed by a BSD-style license
+*  that can be found in the LICENSE file in the root of the source
+*  tree.
+*/
+
+'use strict';
+
+var videoElement = document.querySelector('video');
+var videoSelect = document.querySelector('select#videoSource');
+var selectors = [videoSelect];
+
+function gotDevices(deviceInfos) {
+  // Handles being called several times to update labels. Preserve values.
+  var values = selectors.map(function(select) {
+    return select.value;
+  });
+  selectors.forEach(function(select) {
+    while (select.firstChild) {
+      select.removeChild(select.firstChild);
+    }
+  });
+  for (var i = 0; i !== deviceInfos.length; ++i) {
+    var deviceInfo = deviceInfos[i];
+    var option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || 'camera ' + (videoSelect.length + 1);
+      videoSelect.appendChild(option);
+    } else {
+      console.log('Some other kind of source/device: ', deviceInfo);
+    }
+  }
+  selectors.forEach(function(select, selectorIndex) {
+    if (Array.prototype.slice.call(select.childNodes).some(function(n) {
+      return n.value === values[selectorIndex];
+    })) {
+      select.value = values[selectorIndex];
+    }
+  });
 }
 
-setInterval(function(){
-        context.drawImage(video, 0, 0, 640, 480);
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
 
-},1)
+// Attach audio output device to video element using device/sink ID.
+function attachSinkId(element) {
+
+    element.setSinkId(sinkId)
+    .then(function() {
+      console.log('Success, audio output device attached: ' + sinkId);
+    })
+    .catch(function(error) {
+      var errorMessage = error;
+      if (error.name === 'SecurityError') {
+        errorMessage = 'You need to use HTTPS for selecting audio output ' +
+            'device: ' + error;
+      }
+      console.error(errorMessage);
+
+    });
+  
+}
+
+function changeAudioDestination() {
+
+  attachSinkId(videoElement);
+}
+
+function gotStream(stream) {
+  window.stream = stream; // make stream available to console
+  videoElement.srcObject = stream;
+  // Refresh button list in case labels have become available
+  return navigator.mediaDevices.enumerateDevices();
+}
+
+function start() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(function(track) {
+      track.stop();
+    });
+  }
+  var videoSource = videoSelect.value;
+  var constraints = {
+    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+  };
+  navigator.mediaDevices.getUserMedia(constraints).
+      then(gotStream).then(gotDevices).catch(handleError);
+}
+
+videoSelect.onchange = start;
+
+start();
+
+function handleError(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+
+// Grab elements, create settings, etc.
+// var video = document.getElementById('video');
+// var canvas = document.getElementById('canvas');
+// var context = canvas.getContext('2d');
+
+
+// // Get access to the camera!
+// if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+//     // Not adding `{ audio: true }` since we only want video now
+//     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+//         video.src = window.URL.createObjectURL(stream);
+//         video.play()
+//         // context.drawImage(video, 0, 0, 640, 480);
+//         console.info(video,"video--->");        
+        
+//     });
+// }
+
+// setInterval(function(){
+//         context.drawImage(video, 0, 0, 640, 480);
+
+// },1)
 // Trigger photo take
 // document.getElementById("snap").addEventListener("click", function() {
 //         console.info(video,"video");
