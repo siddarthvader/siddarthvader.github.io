7b4607360f25972a308eafab6a711f2e9974e6db
siddharth <siddharthjn93@gmail.com>
2017-01-23 22:22:43 +0530

    video added canvas

---
+++ demo/demo.js
@@ -0,0 +1,25 @@
+// Grab elements, create settings, etc.
+var video = document.getElementById('video');
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        video.src = window.URL.createObjectURL(stream);
+        video.play()
+        // context.drawImage(video, 0, 0, 640, 480);
+        console.info(video,"video--->");        
+        
+    });
+}
+
+setInterval(function(){
+        context.drawImage(video, 0, 0, 640, 480);
+
+},1)
+// Trigger photo take
+// document.getElementById("snap").addEventListener("click", function() {
+//         console.info(video,"video");
+	
+// });
