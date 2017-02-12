d7c1c6ebfc1621c8a22338b863f8dbe38cd4d93f
siddharth <siddharthjn93@gmail.com>
2017-02-12 21:58:10 +0530

    pro pag ad

--- ionic/vismyra/www/app/config.js
+++ ionic/vismyra/www/app/config.js
@@ -25,6 +25,30 @@ vapp.config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvide
                 controllerAs:'sideMenu'    
             }
         }
+    }).state('product', {
+        //    templateUrl:'app/templates/header.html',
+        views: {
+            'header': {
+                templateUrl: 'app/templates/header.html',
+                controller: 'headerCtrl',
+                controllerAs: 'header'
+            },
+            'content':{
+                templateUrl:'app/templates/product.html',
+                controller:'productCtrl',
+                controllerAs:'product'
+            },
+            'footer':{
+                templateUrl:'app/templates/footer.html',
+                controller:'footerCtrl',
+                controllerAs:'footer'
+            },
+            'side-menu':{
+                templateUrl:'app/templates/side-menu.html',
+                controller:'sideMenuCtrl',
+                controllerAs:'sideMenu'    
+            }
+        }
     });
 
 }]);
