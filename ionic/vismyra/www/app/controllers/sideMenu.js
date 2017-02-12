2de11b454aa353964c990321ff13508b592d43f9
siddharth <siddharthjn93@gmail.com>
2017-02-12 19:12:01 +0530

    toggle added

--- ionic/vismyra/www/app/controllers/sideMenu.js
+++ ionic/vismyra/www/app/controllers/sideMenu.js
@@ -1,9 +1,10 @@
-vapp.controller('sideMenuCtrl', ['$scope','$rootScope', function ($scope,$rootScope) {
+vapp.controller('sideMenuCtrl', ['$scope','$rootScope', function ($scope,$rootScope,$ionicSideMenuDelegate) {
     var sideMenu = this;
     $scope.text = 'Powered by Bailfire Labs';
     $scope.categories = ['Wall clock', 'Wooden keyholder', 'Spirtual Art', 'Wall hangings'];
     $scope.showCategory = function (cat) {
-          $rootScope.category=cat;  
+          $rootScope.category=cat;
+        //   $ionicSideMenuDelegate.toggleLeft(); 
     }
 
 }]);
