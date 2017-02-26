e808c79098e30a8de79062efb2351a79a1012e9b
siddharth <siddharthjn93@gmail.com>
2017-02-27 02:11:49 +0530

    one more

--- ionic/vismyra/www/app/controllers/playground.js
+++ ionic/vismyra/www/app/controllers/playground.js
@@ -1,77 +1,24 @@
 vapp.controller('playgroundCtrl', ['$scope', '$rootScope', '$state', '$ionicScrollDelegate', '$cordovaCamera', function ($scope, $rootScope, $state, $ionicScrollDelegate, $cordovaCamera) {
-    console.info('we full here');
 
-    $scope.$on('$viewContentLoaded', function () {
-        var videoElement = document.querySelector('video');
-        var audioSelect = document.querySelector('select#audioSource');
-        var videoSelect = document.querySelector('select#videoSource');
+    $scope.takeImage = function () {
+        var options = {
+            quality: 80,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            allowEdit: true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 250,
+            targetHeight: 250,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false
+        };
 
-        navigator.getUserMedia = navigator.getUserMedia ||
-            navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-        function gotSources(sourceInfos) {
-            for (var i = 0; i !== sourceInfos.length; ++i) {
-                var sourceInfo = sourceInfos[i];
-                var option = document.createElement('option');
-                option.value = sourceInfo.id;
-                if (sourceInfo.kind === 'audio') {
-                    option.text = sourceInfo.label || 'microphone ' +
-                        (audioSelect.length + 1);
-                    audioSelect.appendChild(option);
-                } else if (sourceInfo.kind === 'video') {
-                    option.text = sourceInfo.label || 'camera ' + (videoSelect.length + 1);
-                    videoSelect.appendChild(option);
-                } else {
-                    console.log('Some other kind of source: ', sourceInfo);
-                }
-            }
-        }
-
-        if (typeof MediaStreamTrack === 'undefined' ||
-            typeof MediaStreamTrack.getSources === 'undefined') {
-            alert('This browser does not support MediaStreamTrack.\n\nTry Chrome.');
-        } else {
-            MediaStreamTrack.getSources(gotSources);
-        }
-
-        function successCallback(stream) {
-            window.stream = stream; // make stream available to console
-            videoElement.src = window.URL.createObjectURL(stream);
-            videoElement.play();
-        }
-
-        function errorCallback(error) {
-            console.log('navigator.getUserMedia error: ', error);
-        }
-
-        function start() {
-            if (window.stream) {
-                videoElement.src = null;
-                window.stream.stop();
-            }
-            var audioSource = audioSelect.value;
-            var videoSource = videoSelect.value;
-            var constraints = {
-                audio: {
-                    optional: [{
-                        sourceId: audioSource
-                    }]
-                },
-                video: {
-                    optional: [{
-                        sourceId: videoSource
-                    }]
-                }
-            };
-            navigator.getUserMedia(constraints, successCallback, errorCallback);
-        }
-
-        audioSelect.onchange = start;
-        videoSelect.onchange = start;
-
-        start();
-
-    });
+        $cordovaCamera.getPicture(options).then(function (imageData) {
+            $scope.srcImage = "data:image/jpeg;base64," + imageData;
+        }, function (err) {
+            // error
+        });
+    }
 
     $scope.goToProduct = function () {
         $state.go('product');
