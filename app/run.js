93933a7a3f3744e183834cf7520bf3dcdda99091
siddharth <siddharthjn93@gmail.com>
2017-03-27 21:31:18 +0530

    animation changes

--- app/run.js
+++ app/run.js
@@ -1,6 +1,12 @@
 // we set app here
 
-syd.run(function($state) {
+syd.run(function($state,$timeout) {
     console.info('iadhr');
+
+    $timeout(function(){
+        document.getElementById('target-elem').classList.add('element');
+    },5000)
+    
+
     $state.go('ground.about');
 });
