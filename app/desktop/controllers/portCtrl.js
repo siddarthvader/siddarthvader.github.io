f61ead3da230f082e088ec7e9667685b65053cda
siddharth <siddharthjn93@gmail.com>
2017-03-29 02:06:35 +0530

    minor changes

--- app/desktop/controllers/portCtrl.js
+++ app/desktop/controllers/portCtrl.js
@@ -1,32 +1,3 @@
 syd.controller('portCtrl',function($scope,$state) {
     // console.info('unreal fast');
-    var deterClass=function(){
-        $scope.loaderPos ='';
-        switch($state.current.name){
-            case 'ground.work':{
-                $scope.loaderPos='l20';
-                break;
-            }
-            case 'ground.about':{
-                $scope.loaderPos='l0';
-                 break;
-            }
-            case 'ground.contact':{
-                $scope.loaderPos='l60';
-                 break;
-            }
-            case 'ground.blog':{
-                $scope.loaderPos='l40';
-                 break;
-            }
-            case 'ground.readme':{
-                $scope.loaderPos='l80';
-                 break;
-            }
-        }
-    }
-
-    $scope.$on('$stateChangeSuccess',function(){
-        deterClass();
-    });
 })
--- app/run.js
+++ app/run.js
@@ -1,16 +1,46 @@
 // we set app here
 
-syd.run(function($state,$timeout,$rootScope) {
+syd.run(function ($state, $timeout, $rootScope) {
     console.info('iadhr');
 
-    $timeout(function(){
+    $timeout(function () {
         document.getElementById('target-elem').classList.add('element');
-    },12000)
+    }, 12000)
 
-    $rootScope.$on('$stateChangeSuccess',function(){
-        document.body.scrollTop=0;
+
+
+    $rootScope.$state = $state;
+    var deterClass = function () {
+        $rootScope.loaderPos = '';
+        switch ($state.current.name) {
+            case 'ground.work': {
+                $rootScope.loaderPos = 'l20';
+                break;
+            }
+            case 'ground.about': {
+                $rootScope.loaderPos = 'l0';
+                break;
+            }
+            case 'ground.contact': {
+                $rootScope.loaderPos = 'l60';
+                break;
+            }
+            case 'ground.blog': {
+                $rootScope.loaderPos = 'l40';
+                break;
+            }
+            case 'ground.readme': {
+                $rootScope.loaderPos = 'l80';
+                break;
+            }
+        }
+    }
+
+    $rootScope.$on('$stateChangeSuccess', function () {
+        document.body.scrollTop = 0;
+        deterClass();
+        
     });
-    
 
     $state.go('ground.about');
 });
